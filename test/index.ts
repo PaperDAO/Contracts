0e428d5ead50bcbc55878668641e454eedbcbf05
Roy <toledoroy@gmail.com>
2022-06-04 12:48:00 -0700

    Cleanup

--- test/index.ts
+++ test/index.ts
@@ -4,24 +4,6 @@ import { Contract, ContractReceipt, Signer } from "ethers";
 
 let WPContract: Contract;
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
-
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
-
-
 describe("Paper NFT", function () {
   before(async function () {
     WPContract = await ethers.getContractFactory("Whitepaper").then(res => res.deploy());
