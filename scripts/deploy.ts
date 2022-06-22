c7501e7cccf16b889409a00223bcb7c3c3a0fd2f
Roy <toledoroy@gmail.com>
2022-06-22 10:23:45 -0700

    Deployment Script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Contract = await ethers.getContractFactory("Whitepaper");
+  const greeter = await Contract.deploy();
 
   await greeter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("Whitepaper deployed to:", greeter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
